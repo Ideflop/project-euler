d0f73fa4b20a6d03ff936f5e59a353b5c4a5b1e0
Ideflop <vi03pl@gmail.com>
2023-02-21 21:49:16 +0100

    Power digit sum

--- Power_digit_sum.rs
+++ Power_digit_sum.rs
@@ -1,4 +1,31 @@
 fn main() {
-    let pow = 2usize.pow(1000);
-    println!("{pow}");
+    
+    let mut pow = 2u128.pow(100);
+    let mut digit = Vec::new();
+    while pow > 0 {
+        digit.push(pow % 10);
+        pow /= 10;
+    }
+
+    for i in 0..900 {
+        let mut for_the_next = 0;
+        for j in digit.iter_mut() {
+            *j *= 2;
+            *j += for_the_next;
+            if *j > 9 {
+                for_the_next = *j / 10;
+                *j %= 10;
+            } else {
+                for_the_next = 0;
+            }
+        }
+        if for_the_next > 0 {
+            digit.push(for_the_next);
+            for_the_next = 0;
+        }
+    }
+    
+    let tot = digit.iter().map(|c| c).sum::<u128>();
+    println!("{digit:?}");
+    println!("{tot}");
 }
